6a3fa784d05b7028002e309165d9d8b31334d98b
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2024-05-22 21:04:53 +0900

    update

--- ebot.rb
+++ ebot.rb
@@ -13,9 +13,10 @@ BOT_NAME = "ebot/#{MODEL}@#{`hostname -s`.strip}"
 PROMPT_EXTRA = "\n\nAnswer in less than 1900 characters. Finalize with a small question."
 MIN_THREADS = 1
 MAX_THREADS = 1
-MAX_QUEUE = 2
+MAX_QUEUE = 50
 CONTROL_SEQUENCE = "CONTROL"
 CONTROL_REGEXP = /^#{CONTROL_SEQUENCE}/
+SLEEP_TIME = 200
 
 $ollama = Ollama.new(
   credentials: { address: 'http://localhost:11434' },
@@ -24,10 +25,18 @@ $ollama = Ollama.new(
 
 $bot = Discordrb::Bot.new(token: BOT_TOKEN)
 
-$task_queue = Concurrent::ThreadPoolExecutor.new(
+$executor = Concurrent::ThreadPoolExecutor.new(
   min_threads: MIN_THREADS, max_threads: MAX_THREADS, 
-  max_queue: MAX_QUEUE, fallback_policy: :discard
+  max_queue: MAX_QUEUE, fallback_policy: :discard,
 )
+
+$list = []
+
+def task_id
+  "(#{BOT_NAME} task ##{$executor.completed_task_count} " + 
+  "#{$executor.queue_length + MAX_THREADS}/" +
+  "#{$executor.max_queue + $executor.max_length})"
+end
 
 $bot.ready {
   msg = "#{CONTROL_SEQUENCE} #{BOT_NAME} is up."
@@ -36,29 +45,65 @@ $bot.ready {
   $bot.update_status('online', nil, nil)
 }
 
-$bot.message {|e|
+$bot.message(:channel => CHANNEL_ID) {|e|
   if e.channel.id != CHANNEL_ID
-    $stderr.print "returning because #{e.channel.id} is not #ebot.\n"
+    $stderr.print "returning because #{e.channel.id} is not #ebot. (#{task_id})\n"
     next
   end
   if CONTROL_REGEXP.match(e.content)
-    $stderr.print "ignored \"#{e.content}\" because it is a CONTROL.\n"
+    $stderr.print "ignored \"#{e.content}\" because it is a CONTROL. (#{task_id})\n"
     next
   end
-  if $task_queue.queue_length == $task_queue.max_queue
-    msg = "ignored \"#{e.content}\" because the queue is full.\n"
+  if $executor.queue_length == $executor.max_queue
+    msg = "ignored \"#{e.content}\" because the queue is full. (#{task_id})\n"
     $stderr.print "#{msg}\n"
     e.respond("#{CONTROL_SEQUENCE} #{msg}")
     next
   end
-  $task_queue.post do
+  $executor.post do
     $bot.update_status('dnd', nil, nil)
-    e.respond(ask(e.content))
+    if $executor.queue_length < $executor.max_queue / 2
+      $list.push(e)
+      e.respond(ask())
+    else
+      if rand(2) == 0
+        e.respond("CONTROL Skipped because max_queue was hit. #{task_id}" )
+      else
+        $list.push(e)
+        e.respond(ask())
+      end
+    end
     $bot.update_status('online', nil, nil)
   end
 }
 
-def ask(prompt)
+def choose
+  # take oldest non-bot message of oldest bot message
+  event = nil
+  $list.each {|e|
+    if !e.author.bot_account
+      event = e
+      $list.delete(e)
+      break
+    end
+  }
+  event ||= $list.shift
+  $stderr.print "list size: #{$list.size} except this.\n"
+  $stderr.print "author username: #{event.author.username}\n"
+#  $stderr.print "author bot_account?: #{event.author.bot_account}\n"
+#  $stderr.print "channel: #{event.channel}\n"
+#  $stderr.print "content: #{event.content}\n"
+#  $stderr.print "file: #{event.file}\n"
+#  $stderr.print "message: #{event.message}\n"
+#  $stderr.print "server: #{event.server}\n"
+#  $stderr.print "timestamp: #{event.timestamp}\n"
+#  $stderr.print "bot: #{event.bot}\n"
+  event
+end
+
+def ask
+  event = choose
+  prompt = event.content
   question = "#{prompt.sub(/\(ebot.*\)$/, '')} #{PROMPT_EXTRA}"
   $stderr.print "Question: #{question}\n\n"
   s = ""
@@ -70,7 +115,7 @@ def ask(prompt)
     s += event['response']
   }
   $stderr.print "\n\n\n"
-  "#{s} (#{BOT_NAME} #{$task_queue.queue_length + 1}/#{$task_queue.max_queue + $task_queue.max_length})"
+  "#{s} #{task_id}"[0..1999] 
 end
 
 $bot.run
